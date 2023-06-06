94d397d1da2d4462fa7ddb1f422ec2d7414725d8
Dhyey Shah <dhyey.shah45@gmail.com>
2023-06-06 21:01:58 +0530

    added updated origin

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://yay-script.netlify.app",
   })
 );
 app.use(express.urlencoded({ extended: true }));
